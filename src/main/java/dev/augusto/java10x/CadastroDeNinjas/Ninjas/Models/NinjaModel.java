0f80f3317b3324fcb9861f9a955c41cfe8f83ac8
Augusto Honorata <contatoaugustohonorata@gmail.com>
2025-02-23 14:23:06 -0300

    feat: add lombok

--- src/main/java/dev/augusto/java10x/CadastroDeNinjas/Ninjas/Models/NinjaModel.java
+++ src/main/java/dev/augusto/java10x/CadastroDeNinjas/Ninjas/Models/NinjaModel.java
@@ -2,9 +2,15 @@ package dev.augusto.java10x.CadastroDeNinjas.Ninjas.Models;
 
 import dev.augusto.java10x.CadastroDeNinjas.Missoes.Models.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -17,43 +23,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel(Long id, String nome, String email, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
