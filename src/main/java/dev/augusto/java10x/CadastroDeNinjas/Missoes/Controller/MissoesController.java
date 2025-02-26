0eb6c318235118e79c89a37674eec6c379de5d42
Augusto Honorata <contatoaugustohonorata@gmail.com>
2025-02-26 20:45:47 -0300

    fix: change Ninjas and Missoes routes

--- src/main/java/dev/augusto/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
+++ src/main/java/dev/augusto/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
@@ -1,4 +1,33 @@
 package dev.augusto.java10x.CadastroDeNinjas.Missoes.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    @PostMapping("/create")
+    public String createMission(){
+        return "Missao criada";
+    }
+
+    @PutMapping("/update")
+    public String updateMission(){
+        return "Missao Atualizada";
+    }
+
+    @GetMapping("/get-by-id")
+    public String getMissioById(){
+        return "Missao pelo id";
+    }
+
+    @GetMapping("/list-all")
+    public String getMission(){
+        return "Missoes listadas";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteMission(){
+        return "Missao deletada";
+    }
 }
--- src/main/java/dev/augusto/java10x/CadastroDeNinjas/Ninjas/Controllers/NinjaController.java
+++ src/main/java/dev/augusto/java10x/CadastroDeNinjas/Ninjas/Controllers/NinjaController.java
@@ -1,14 +1,37 @@
 package dev.augusto.java10x.CadastroDeNinjas.Ninjas.Controllers;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+    @PostMapping("/create")
+    public String createNinja(){
+        return "Ninja criado";
+    }
+
+    @GetMapping("/get-by-id")
+    public String getNinjaById(){
+        return "Mostrar Ninja por id";
+    }
+
+    @GetMapping("/list-all")
+    public String getAllNinjas(){
+        return "Todos os Ninjas";
+    }
+
+    @PutMapping("/update")
+    public String updateNinja(){
+        return "Altera Ninja";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteNinja(){
+        return "Ninja deletado";
     }
 }
